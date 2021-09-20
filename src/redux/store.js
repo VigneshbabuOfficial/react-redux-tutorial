b79fa3be56deb3c8449a589e6512a08383ceb624
VigneshbabuA <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-09-21 01:59:13 +0530

    react-redux with redux-devtools explained- npm install --save redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./RootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 console.log("store is called");
 
 export default store;
