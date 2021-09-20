af28b053bd034ec2dee006242e1c061e0fdabcb2
VigneshbabuA <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-09-21 01:50:41 +0530

    react-redux with loggers explained

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import { rootReducer } from "./RootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("store is called");
 
 export default store;
