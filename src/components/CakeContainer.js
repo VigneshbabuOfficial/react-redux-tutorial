9575f044f5260f1386b577768b50b86395678e03
VigneshbabuA <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-09-21 00:12:45 +0530

    react-redux flow explained with hooks useSelector and useDispatch

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { buyCake } from "../redux/cake/cakeActions";
-import { connect } from "react-redux";
+import { connect, useSelector, useDispatch } from "react-redux";
 
-function CakeContainer({ numOfCakes, buyCakes }) {
+function CakeContainer() {
   console.log("CakeContainer is called");
+
+  const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h2>Num of Cakes - {numOfCakes}</h2>
-      <button onClick={buyCakes}>Buy Cake</button>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  console.log("CakeContainer - mapStateToProps is called");
-  return {
-    numOfCakes: state.numOfCakes,
-  };
-};
+// const mapStateToProps = (state) => {
+//   console.log("CakeContainer - mapStateToProps is called");
+//   return {
+//     numOfCakes: state.numOfCakes,
+//   };
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  console.log("CakeContainer - mapDispatchToProps is called");
-  return {
-    buyCakes: () => dispatch(buyCake()),
-  };
-};
+// const mapDispatchToProps = (dispatch) => {
+//   console.log("CakeContainer - mapDispatchToProps is called");
+//   return {
+//     buyCakes: () => dispatch(buyCake()),
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
+export default CakeContainer;
+// export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
