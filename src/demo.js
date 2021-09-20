8bfafea287961749f70d8b0b12da58c52972359f
VigneshbabuA <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-09-20 16:14:51 +0530

    redux basics explained

---
+++ src/demo.js
@@ -0,0 +1,92 @@
+const redux = require("redux");
+const reduxLogger = require("redux-logger");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
+
+// action types
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+// action creators
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+// action creators
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE: return {
+//       ...state,
+//       numOfCakes: state.numOfCakes - 1
+//     }
+//     case BUY_ICECREAM: return {
+//       ...state,
+//       numOfIceCreams: state.numOfIceCreams - 1
+//     }
+//     default: return state
+//   }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+// creating store
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+console.log("Initial State ", store.getState());
+
+const unsubscribe = store.subscribe(() => {});
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+
+unsubscribe();
